d000852f4b8aa9db43d0a063cd5679446d1c0dc3
lemcode1 <114827935+lemcode1@users.noreply.github.com>
2022-12-09 11:03:50 +0530

    Update app.module.ts

--- smart_cab_ui/src/app/app.module.ts
+++ smart_cab_ui/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -25,6 +26,7 @@ import { RegisterComponent } from './register/register.component';
   ],
   
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     FlexLayoutModule ,
